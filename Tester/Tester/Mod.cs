6dcb8f79a219a0fa532f0835cc59f1f5a3290570
KralPolska <kralpolska@gmail.com>
2026-06-01 09:29:13 +0200

    World tests
    World tests trying to spawn object

--- Tester/Tester/Mod.cs
+++ Tester/Tester/Mod.cs
@@ -8,8 +8,14 @@ using ReadyM.Api.Multiplayer.Generators;
 using ReadyM.Api.Multiplayer.Protocol.Enums;
 using ReadyM.Api.Multiplayer.RPC;
 using ReadyM.Api.Multiplayer.Serialization;
+using ReadyM.Wukong.Common.ECS.Components;
+using ReadyM.Wukong.Common.ECS.Values;
 using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
 using Tester;
 using UnrealEngine.Engine;
 using UnrealEngine.Runtime;
@@ -32,5 +38,44 @@ public class Mod : ModBase
         services.RegisterSingleton<TesterService>();
         PlayerTester.invoke();
         Spawner.Init();
+        //WukongApi.Input.RegisterKeyBind(Key.F9, WorldTester.ReckonActors);
+        WukongApi.Input.RegisterKeyBind(Key.F9, () => 
+        {
+            try
+            {
+                var location = WukongApi.Sync.LocalMainCharacter?.Location;
+                var rotation = WukongApi.Sync.LocalMainCharacter?.Rotation;
+
+                //Spawn actors next to the player
+                WorldTester.SpawnActor("BPO_TreasureBox_03a_C", 
+                    new FVector((double)location?.X + 300, (double)location?.Y, (double)location?.Z), 
+                    new FRotator((double)rotation?.X, (double)rotation?.Y, (double)rotation?.Z));
+                WorldTester.SpawnActor("BPO_TreasureBox_04a_C", 
+                    new FVector((double)location?.X - 300, (double)location?.Y, (double)location?.Z), 
+                    new FRotator((double)rotation?.X, (double)rotation?.Y, (double)rotation?.Z));
+                WorldTester.SpawnActor("BPO_TreasureBox_06_C", 
+                    new FVector((double)location?.X, (double)location?.Y + 300, (double)location?.Z), 
+                    new FRotator((double)rotation?.X, (double)rotation?.Y, (double)rotation?.Z));
+                WorldTester.SpawnActor("BPO_TreasureBox_08_C", 
+                    new FVector((double)location?.X, (double)location?.Y - 300, (double)location?.Z), 
+                    new FRotator((double)rotation?.X, (double)rotation?.Y, (double)rotation?.Z));
+                WorldTester.TryChangeActors();
+            }
+            catch (Exception ex)
+            {
+                WukongApi.Chat.ShowLocalMessage("Error spawning actors: " + ex.Message, FLinearColor.Red);
+            }
+        });
+        WukongApi.Input.RegisterKeyBind(Key.F8, () =>
+        {
+            if (WorldTester.TryGetMyPlayerActor() is not { } player)
+            {
+                WukongApi.Chat.ShowLocalMessage("Could not find player actor", FLinearColor.Red);
+                return;
+            }
+
+            WukongApi.Chat.ShowLocalMessage($"Player hidden: {player.Hidden}, setting to {!player.Hidden}", FLinearColor.Yellow);
+            player.SetActorHiddenInGame(!player.Hidden);
+        });
     }
 }
---
+++ Tester/Tester/WorldTester.cs
@@ -0,0 +1,123 @@
+﻿using CSharpModBase;
+using CSharpModBase.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnrealEngine.Engine;
+using UnrealEngine.Runtime;
+using WukongMp.Api;
+using WukongMp.Api.WukongUtils;
+using WukongMp.Sdk.Api;
+using WukongMp.Sdk.Entities;
+
+namespace Tester
+{
+
+    public static class WorldTester
+    {
+        public static List<AActor> MyActors = new List<AActor>();
+
+        public static void ReckonActors()
+        {
+            HashSet<string> actorNames = new HashSet<string>();
+
+            var world = GameUtils.GetWorld();
+            if (world == null)
+            {
+                return;
+            }
+
+            var allActors = world.GetAllActorsOfClass<AActor>();
+            foreach (var actor in allActors)
+            {
+                if (actor != null)
+                {
+                    actorNames.Add(actor.GetClass().GetName());
+                }
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktopPath, "WukongActorsClassList.txt");
+
+            try
+            {
+                File.WriteAllLines(filePath, actorNames.OrderBy(n => n));
+                
+                WukongApi.Chat.ShowLocalMessage($"Saved {actorNames.Count} unique actor classes", FLinearColor.Green);
+                WukongApi.Chat.ShowLocalMessage($"Actor list saved to: {filePath}", FLinearColor.Green);
+            }
+            catch (Exception ex)
+            {
+                Logging.LogError($"Error: {ex.Message}");
+            }
+        }
+
+        public static void SpawnActor(string actorClassName, FVector location, FRotator rotation)
+        {
+            UClass actorClass = UClass.GetClass(actorClassName);
+            if (actorClass == null)
+            {
+                WukongApi.Chat.ShowLocalMessage("No actor class named: " + actorClassName, FLinearColor.Red);
+                return;
+            }
+
+            UWorld? world = GameUtils.GetWorld();
+            if (world == null)
+            {
+                WukongApi.Chat.ShowLocalMessage("No world found", FLinearColor.Red);
+                return;
+            }
+
+            MyActors.Add(world.SpawnActor(actorClass, ref location, ref rotation));
+        }
+
+        public static void TryChangeActors()
+        {
+            if (MyActors.Count == 0)
+                return;
+
+            var myActor = MyActors[0];
+            myActor.SetActorEnableCollision(false);
+            myActor.SetActorHiddenInGame(true);
+            MyActors[1].OnTakeAnyDamage.Bind(OnMyActorDamaged);
+        }
+
+        public static AActor? TryGetMyPlayerActor()
+        {
+            try
+            {
+                var world = GameUtils.GetWorld();
+                if (world == null)
+                {
+                    WukongApi.Chat.ShowLocalMessage("No world found", FLinearColor.Red);
+                    return null;
+                }
+
+                var playerPawn = world.GetPlayerPawn(0);
+                if (playerPawn == null)
+                {
+                    WukongApi.Chat.ShowLocalMessage("No player pawn 0 found", FLinearColor.Red);
+                    playerPawn = world.GetPlayerPawn(1);
+                    if (playerPawn == null)
+                    {
+                        WukongApi.Chat.ShowLocalMessage("No player pawn 1 found", FLinearColor.Red);
+                        return null;
+                    }
+                }
+                return playerPawn;
+            }
+            catch (Exception ex)
+            {
+                WukongApi.Chat.ShowLocalMessage("Error getting player actor: " + ex.Message, FLinearColor.Red);
+                return null;
+            }
+        }
+
+        private static void OnMyActorDamaged(AActor DamagedActor, float Damage, UDamageType DamageType, AController InstigatedBy, AActor DamageCauser)
+        {
+            WukongApi.Chat.ShowLocalMessage($"{DamagedActor.GetName()}, took {Damage.ToString()} {DamageType.GetName()} by {DamageCauser.GetName()}", FLinearColor.Red);
+        }
+    }
+}
